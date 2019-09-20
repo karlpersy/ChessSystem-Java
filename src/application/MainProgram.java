d2c03ef3b24ff5f1e71f9313774763a46ad8d500
José Carlito <karpersy@gmail.com>
2019-09-20 05:12:55 -0300

    Criando classe Position

--- src/application/MainProgram.java
+++ src/application/MainProgram.java
@@ -1,10 +1,14 @@
 package application;
 
+import board.Position;
+
 public class MainProgram {
 
 	public static void main(String[] args) {
 		System.out.println("Hello seus CAVALOSS!!!");
-
+		
+        Position posi = new Position (3,5);
+    	System.out.println(posi);
 	}
 
 }
